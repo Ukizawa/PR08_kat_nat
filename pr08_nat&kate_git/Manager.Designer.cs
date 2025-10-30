bbdf7587569a174f77d9868279b16a67c5797a05
krasilnikova <sakanadee@yandex.ru>
2025-10-30 09:30:31 +0300

    поменяли цвета, поменяли иконку, сделали всю работу

--- pr08_nat&kate_git/Manager.Designer.cs
+++ pr08_nat&kate_git/Manager.Designer.cs
@@ -29,12 +29,82 @@ namespace pr08_nat_kate_git
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Manager));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Фильтры = new System.Windows.Forms.ListBox();
+            this.Сортировка = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(581, 333);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(689, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Фильтры
+            // 
+            this.Фильтры.BackColor = System.Drawing.Color.Chartreuse;
+            this.Фильтры.FormattingEnabled = true;
+            this.Фильтры.Location = new System.Drawing.Point(602, 105);
+            this.Фильтры.Name = "Фильтры";
+            this.Фильтры.Size = new System.Drawing.Size(186, 147);
+            this.Фильтры.TabIndex = 2;
+            // 
+            // Сортировка
+            // 
+            this.Сортировка.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Сортировка.FormattingEnabled = true;
+            this.Сортировка.Location = new System.Drawing.Point(602, 269);
+            this.Сортировка.Name = "Сортировка";
+            this.Сортировка.Size = new System.Drawing.Size(186, 160);
+            this.Сортировка.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ПОИСК";
+            // 
+            // Manager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Сортировка);
+            this.Controls.Add(this.Фильтры);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Manager";
+            this.Text = "Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox Фильтры;
+        private System.Windows.Forms.ListBox Сортировка;
+        private System.Windows.Forms.Label label1;
     }
 }
